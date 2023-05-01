b07c3605941aa24a01eb35d1bc953376e83bef9c
liam-1392 <125411449+liam-1392@users.noreply.github.com>
2023-05-01 07:34:57 -0700

    All of the files
    A small game I have been working on

---
+++ Scenes/Landscape/Pickup.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var playerEntered = false
+export var drop = ""
+export var quantity = 0
+
+func _ready():
+	add_to_group("GameState")
+
+func CheckDestroy():
+	if playerEntered:
+		print("clicked")
+		print("destroyed")
+		queue_free()
+
+func IsRayCastEntered():
+	$Timer.start()
+	playerEntered = true
+
+func _on_Timer_timeout():
+	playerEntered = false
